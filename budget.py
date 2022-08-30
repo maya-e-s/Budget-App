5ecf35014ac3babd16983a25e433bb6c40b894ff
Maya Stevenson <maya.stevenson@gmail.com>
2022-08-30 14:46:27 -0600

    Add class Category

--- budget.py
+++ budget.py
@@ -1,7 +1,46 @@
 class Category:
+    def __init__(self, cat):
+        self.cat = cat # category such as food, clothing, entertainment
+        self.ledger = list() # list of dict in format of {"amount": amount, "description": description}
+        self.funds = 0.0
+    
+    # Format how the budget object is printed
+    def __str__(self):
+        ret = self.cat.center(30, '*') + "\n"
+        for item in self.ledger:
+            amount = "{:.2f}".format(item.get('amount'))
+            line = (item.get('description'))[:23].ljust(23, ' ') + amount.rjust(7, ' ')
+            ret += line + '\n'
+        ret += "Total: " + "{:.2f}".format(self.funds)
+        return ret
 
+    def deposit(self, amount, desc=''):
+        self.ledger.append({'amount':amount, 'description':desc})
+        self.funds += amount
 
+    # return True if the withdrawal took place, and False otherwise
+    def withdraw(self, amount, desc=''):
+        if self.check_funds(amount): # if there are not enough funds, nothing should be added to the ledger
+            self.ledger.append({'amount':-1*amount, 'description':desc})
+            self.funds -= amount
+            return True
+        else: return False
 
+    # return True if the transfer took place, and False otherwise
+    def transfer(self, amount, category_inst):
+        desc1 = "Transfer to " + category_inst.cat
+        desc2 = "Transfer from " + self.cat
+        if self.withdraw(amount, desc1): # if there are not enough funds, nothing should be added to either ledgers
+            category_inst.deposit(amount, desc2)
+            return True
+        return False
 
+    # returns False if the amount is greater than the balance of the budget category and returns True otherwise
+    def check_funds(self, amount): 
+        if amount > self.funds: return False
+        return True 
 
-def create_spend_chart(categories):
+    def get_balance(self):
+        return self.funds
+
+#def create_spend_chart(categories):
